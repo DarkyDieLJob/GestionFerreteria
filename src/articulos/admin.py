8182e77d28d31c690ef86ca440bb6ff945e25162
DarkyDieLJob <diel.job@gmail.com>
2025-08-19 11:16:51 -0300

    fix(admin): ensure adapter models are imported before auto-registering models

--- src/articulos/admin.py
+++ src/articulos/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.apps import apps
 
 # Registrar automáticamente todos los modelos de la app "articulos"
+from .adapters import models as _models  # force import of models in adapters
 app_config = apps.get_app_config("articulos")
 for model in app_config.get_models():
     try:
